7ae2e0b3b773710b19197e0386bbc87c37faa9fd
Daniyal Naeem <daniyalnaeem5@gmail.com>
2021-06-23 14:43:19 +0500

    live location added

--- screens/ChooseLocation.js
+++ screens/ChooseLocation.js
@@ -1,61 +1,116 @@
-import React, {useState, useRef} from 'react';
-import {View, StyleSheet} from 'react-native';
-import MapView, {Marker} from 'react-native-maps';
+import React, {useState, useRef, useEffect} from 'react';
+import {View, StyleSheet, Dimensions} from 'react-native';
+import MapView, {Marker, AnimatedRegion} from 'react-native-maps';
 import {GOOGLE_MAP_KEY} from '../consts/googleMapKey';
 import MapViewDirections from 'react-native-maps-directions';
+import imagePath from '../consts/imagePath';
+import {locationPermission, getCurrentLocation} from '../helper/helperFunction';
+
+const screen = Dimensions.get('screen');
+const ASPECT_RATIO = screen.width / screen.height;
+const LATITUDE_DELTA = 0.9222;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
 
 const ChooseLocation = () => {
   const mapRef = useRef();
+
   const [state, setState] = useState({
-    pickupCords: {
-      latitude: 34.025917,
-      longitude: 71.560135,
-      latitudeDelta: 0.0922,
-      longitudeDelta: 0.0421,
+    curLoc: {
+      latitude: 34.0151,
+      longitude: 71.5249,
     },
-    dropCords: {
-      latitude: 34.015858,
-      longitude: 71.975449,
-      latitudeDelta: 0.0922,
-      longitudeDelta: 0.0421,
-    },
+    destinationCords: {},
+    coordinate: new AnimatedRegion({
+      latitude: 34.0151,
+      longitude: 71.5249,
+      latitudeDelta: LATITUDE_DELTA,
+      longitudeDelta: LONGITUDE_DELTA,
+    }),
   });
-  const {pickupCords, dropCords} = state;
+
+  const {curLoc, destinationCords, coordinate} = state;
+
+  useEffect(() => {
+    getLiveLocation();
+  }, []);
+
+  const getLiveLocation = async () => {
+    const locPermissionDenied = await locationPermission();
+    if (locPermissionDenied) {
+      const {latitude, longitude} = await getCurrentLocation();
+      console.log('get live location after 4 second');
+      animate(latitude, longitude);
+      setState({
+        ...state,
+        curLoc: {latitude, longitude},
+        coordinate: new AnimatedRegion({
+          latitude: latitude,
+          longitude: longitude,
+          latitudeDelta: LATITUDE_DELTA,
+          longitudeDelta: LONGITUDE_DELTA,
+        }),
+      });
+    }
+  };
+  useEffect(() => {
+    const interval = setInterval(() => {
+        getLiveLocation()
+    }, 4000);
+    return () => clearInterval(interval)
+})
   return (
     <View style={styles.container}>
       <View style={{flex: 1}}>
-        <MapView ref={mapRef} style={styles.map} initialRegion={pickupCords}>
-          <Marker coordinate={pickupCords} />
-          <Marker coordinate={dropCords} />
-          <MapViewDirections
-            origin={pickupCords}
-            destination={dropCords}
-            apikey={GOOGLE_MAP_KEY}
-            strokeWidth={3}
-            strokeColor="hotpink"
-            optimizeWaypoints={true}
-            onStart={params => {
-              console.log(
-                `Started routing between "${params.origin}" and "${params.destination}"`,
-              );
-            }}
-            onReady={result => {
-              console.log(`Distance: ${result.distance} km`);
-              console.log(`Duration: ${result.duration} min.`);
-
-              mapRef.current.fitToCoordinates(result.coordinates, {
-                edgePadding: {
-                  right: 30,
-                  bottom: 300,
-                  left: 30,
-                  top: 100,
-                },
-              });
-            }}
-            onError={errorMessage => {
-              // console.log('GOT AN ERROR');
-            }}
+        <MapView
+          ref={mapRef}
+          style={styles.map}
+          initialRegion={{
+            ...curLoc,
+            latitudeDelta: LATITUDE_DELTA,
+            longitudeDelta: LONGITUDE_DELTA,
+          }}>
+          <Marker.Animated
+            coordinate={coordinate}
+            image={imagePath.icCurLoc}
           />
+
+          {Object.keys(destinationCords).length > 0 && (
+            <Marker
+              coordinate={destinationCords}
+              image={imagePath.icGreenMarker}
+            />
+          )}
+          {Object.keys(destinationCords).length > 0 && (
+            <MapViewDirections
+              origin={curLoc}
+              destination={destinationCords}
+              apikey={GOOGLE_MAP_KEY}
+              strokeWidth={6}
+              strokeColor="red"
+              optimizeWaypoints={true}
+              onStart={params => {
+                console.log(
+                  `Started routing between "${params.origin}" and "${params.destination}"`,
+                );
+              }}
+              onReady={result => {
+                console.log(`Distance: ${result.distance} km`);
+                console.log(`Duration: ${result.duration} min.`);
+
+                mapRef.current.fitToCoordinates(result.coordinates, {
+                  edgePadding: {
+                    right: 30,
+                    bottom: 300,
+                    left: 30,
+                    top: 100,
+                  },
+                });
+              }}
+              onError={errorMessage => {
+                // console.log('GOT AN ERROR');
+              }}
+            />
+          )}
         </MapView>
       </View>
     </View>
@@ -70,22 +125,6 @@ const styles = StyleSheet.create({
     ...StyleSheet.absoluteFillObject,
     width: '100%',
     height: '100%',
-  },
-  bottomCard: {
-    backgroundColor: 'white',
-    width: '100%',
-    padding: 30,
-    borderTopEndRadius: 24,
-    borderTopStartRadius: 24,
-  },
-  inpuStyle: {
-    backgroundColor: 'white',
-    borderRadius: 4,
-    borderWidth: 1,
-    alignItems: 'center',
-    height: 48,
-    justifyContent: 'center',
-    marginTop: 16,
   },
 });
 
